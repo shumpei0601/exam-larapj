8a7d849adc0de523a27338aa51317df07dfd7989
@shumpei0601 <s.fxcmcentral1012@gmail.com>
2022-02-06 11:31:43 +0900

    tenth-commit

--- database/seeders/ContactsTableSeeder.php
+++ database/seeders/ContactsTableSeeder.php
@@ -14,6 +14,6 @@ class ContactsTableSeeder extends Seeder
      */
     public function run()
     {
-        Contact::factory()->count(0)->create();
+        Contact::factory()->count(35)->create();
     }
 }
